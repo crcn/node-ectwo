52ff19f15e4e21ffb546fea67e3ab493616eaff3
Craig Condon <craig@spiceapps.com>
2013-02-21 11:31:57 -0800

    update test

--- test/tests/region/tests/image.migrate.test/index.js
+++ test/tests/region/tests/image.migrate.test/index.js
@@ -32,6 +32,45 @@ exports.load = function(region, image, loader, next) {
       }));
     });
 
+    it("all regions have images with vanilla flavored tags", function(done) {
+      async.forEach(tregions, function(region, next) {
+        region.images.find({ tags: { key: "flavor", value: "vanilla" } }, outcome.e(next).s(function(images) {
+          expect(images.length).not.to.be(0);
+          next();
+        }))
+      }, done);
+    });
+
+
+    it("all regions can start an instance", function(done) {
+      async.forEach(tregions, function(region, next) {
+        region.images.findAll(outcome.e(next).s(function(images) {
+          images[0].createInstance(outcome.e(next).s(function(instance) {
+            expect(instance).not.to.be(undefined);
+          }));
+        }));
+      }, done);
+    });
+
+    it("all regions have at least one running instance", function(done) {
+      async.forEach(tregions, function(region, next) {
+        region.instances.find(outcome.e(next).s(function(instances) {
+          expect(instances.length).not.to.be(0);
+          next();
+        }));
+      }, done);
+    });
+
+    it("all regions can remove running instances", function(done) {
+      async.forEach(tregions, function(region, next) {
+        region.instances.find(outcome.e(next).s(function(instances) {
+          async.forEach(instances, function(instance, next) {
+            instance.destroy(next);
+          }, next);
+        }));
+      }, done);
+    });
+
     it("all regions can remove the ported image", function(done) {
       async.forEach(tregions, function(region, next) {
         region.images.findAll(outcome.e(next).s(function(images) {
